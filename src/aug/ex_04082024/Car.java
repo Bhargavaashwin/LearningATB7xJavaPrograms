56e046c01d4f435afc36b29faa12429c8c186fdc
Bhargavaashwin <bhargava3398@gmail.com>
2024-08-29 09:46:12 +0530

    10th Aug part 1 & part 2

---
+++ src/aug/ex_04082024/Car.java
@@ -0,0 +1,13 @@
+package aug.ex_04082024;
+
+public class Car {
+
+    // DC
+    Car() {
+        System.out.println("Starting");
+
+    }
+    void Dog(){
+
+    }
+}
--- src/ex_07072024/Bh.java
+++
@@ -1,7 +0,0 @@
-package ex_07072024;
-
-public class Bh {
-    public static void main(String[] args) {
-        System.out.println("Sample");
-    }
-}
